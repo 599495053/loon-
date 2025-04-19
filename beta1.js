8d0983265c60a37813a21ea5da171d5a20cebfb9
599495053 <52025644+599495053@users.noreply.github.com>
2025-04-20 03:32:50 +0800

    Update beta1.js

--- beta1.js
+++ beta1.js
@@ -56,11 +56,6 @@ function sendConnectRequest() {
 
   const requestHeader = [
     `CONNECT ${host}:${port} HTTP/1.1\r\n`,
-    'Host: 153.3.236.22:443\r\n', // 固定百度代理服务器地址（Lua脚本中的硬编码）
-    'User-Agent: Mozilla/5.0 (Linux; Android 12; RMX3300 Build/SKQ1.211019.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36 T7/13.32 SP-engine/2.70.0 baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0\r\n',
-    'X-WECHAT-OS: Android 12\r\n' + // 模拟系统版本
-'X-WECHAT-DEVICE: RMX3300\r\n' + // 模拟设备型号（原UA中的RMX3300）
-'X-WECHAT-APPVERSION: 8.0.38\r\n' // 模拟微信版本
     'Proxy-Connection: keep-alive\r\n',
     `X-T5-Auth: ${FIXED_AUTH}\r\n\r\n` // 固定验证值，与Lua完全一致
   ].join('');
