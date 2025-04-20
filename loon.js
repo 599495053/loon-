c18caacd4e16710cd19c8fe21b498317196f49a6
599495053 <52025644+599495053@users.noreply.github.com>
2025-04-20 15:51:37 +0800

    Update loon.js

--- loon.js
+++ loon.js
@@ -1,116 +1,99 @@
 /**
- * 百度直连代理协议（JavaScript版）
- * 适配Loon自定义协议
- * 作者：星璃
- * 邮箱：StarColoredGlaze@outlook.com
- * 时间：2023-06-27
+ * 本脚本实现HTTP代理协议，可用于Loon的自定义协议（custom类型）
+ * 使用方式：
+ * [Proxy]
+ * customHttp = custom, remoteAddress, port, script-path=https://raw.githubusercontent.com/Loon0x00/LoonExampleConfig/master/Script/http.js
+ * 
+ * 脚本：
+ * 全局参数 $session 表示当前的一个tcp会话，一个session对象样例
+ * $session = {
+     "uuid":"xxxx",//会话id
+     "type":0,
+     "conHost":"google.com",
+     "conPort":443,
+     "proxy":{
+         "name":"customHttp",
+         "host":"192.168.1.139",
+         "port":"7222",
+         "userName":"username",
+         "password":"password",
+         "encryption":"aes-128",
+         "allowInsecure":false,
+         "ceritificateHost":"",
+         "isTLS":false
+     }
+ }
+ *  实现5个session的生命周期方法
+ *  function tunnelDidConnected(); //会话tcp连接成功回调
+ *  function tunnelTLSFinished(); //会话进行tls握手成功
+ *  function tunnelDidRead(data); //从代理服务器读取到数据回调
+ *  function tunnelDidWrite(); //数据发送到代理服务器成功
+ *  function tunnelDidClose(); //会话已关闭
+ * 
+ *  $tunnel对象，主要用来操作session的一些方法
+ *  $tunnel.write($session, data); //想代理服务器发送数据，data可以为ArrayBuffer也可以为string
+ *  $tunnel.read($session); //从代理服务器读取数据
+ *  $tunnel.readTo($session, trialData); //从代理服务器读取数据，一直读到数据末尾是trialData为止
+ *  $tunnel.established($session); //会话握手成功，开始进行数据转发，一般在协议握手成功后调用
+ *  
  */
 
-const HTTP_STATUS_INVALID = -1;
-const HTTP_STATUS_CONNECTED = 0;
-const HTTP_STATUS_WAITRESPONSE = 1;
-const HTTP_STATUS_FORWARDING = 2;
+let HTTP_STATUS_INVALID = -1;
+let HTTP_STATUS_CONNECTED = 0;
+let HTTP_STATUS_WAITRESPONSE = 1;
+let HTTP_STATUS_FORWARDING = 2;
+var httpStatus = HTTP_STATUS_INVALID;
 
-// 会话状态管理
-const sessionStatus = new Map();
-
-function createVerify(address) {
-  let index = 0;
-  for (let i = 0; i < address.length; i++) {
-    index = (index * 1318293 & 0x7FFFFFFF) + address.charCodeAt(i);
-  }
-  if (index < 0) index &= 0x7FFFFFFF;
-  return index;
-}
-
-// 隧道连接成功回调
-function tunnelDidConnected(session) {
-  console.log(session);
-  const isTLS = session.proxy.isTLS;
-  
-  if (isTLS) {
-    // TLS握手完成后处理
-    tunnelTLSFinished(session);
+function tunnelDidConnected() {
+  console.log($session);
+  if ($session.proxy.isTLS) {
+    //https
   } else {
-    // HTTP模式直接发送头部
-    _writeHttpHeader(session);
-    sessionStatus.set(session.uuid, HTTP_STATUS_CONNECTED);
+    //http
+    _writeHttpHeader();
+    httpStatus = HTTP_STATUS_CONNECTED;
   }
   return true;
 }
 
-// TLS握手完成回调
-function tunnelTLSFinished(session) {
-  _writeHttpHeader(session);
-  sessionStatus.set(session.uuid, HTTP_STATUS_CONNECTED);
+function tunnelTLSFinished() {
+  _writeHttpHeader();
+  httpStatus = HTTP_STATUS_CONNECTED;
   return true;
 }
 
-// 数据读取回调
-function tunnelDidRead(session, data) {
-  const status = sessionStatus.get(session.uuid);
-  
-  if (status === HTTP_STATUS_WAITRESPONSE) {
-    // 检查HTTP响应码
-    if (data.includes('\r\n\r\n')) {
-      const headers = data.slice(0, data.indexOf('\r\n\r\n'));
-      if (headers.includes('HTTP/1.1 200')) {
-        console.log('HTTP握手成功');
-        sessionStatus.set(session.uuid, HTTP_STATUS_FORWARDING);
-        $tunnel.established(session); // 开始数据转发
-        return null; // 不转发数据到客户端
-      }
-    }
-  } else if (status === HTTP_STATUS_FORWARDING) {
+function tunnelDidRead(data) {
+  if (httpStatus == HTTP_STATUS_WAITRESPONSE) {
+    //check http response code == 200
+    //Assume success here
+    console.log('http handshake success');
+    httpStatus = HTTP_STATUS_FORWARDING;
+    $tunnel.established($session); //可以进行数据转发
+    return null; //不将读取到的数据转发到客户端
+  } else if (httpStatus == HTTP_STATUS_FORWARDING) {
     return data;
   }
 }
 
-// 数据发送回调
-function tunnelDidWrite(session) {
-  const status = sessionStatus.get(session.uuid);
-  
-  if (status === HTTP_STATUS_CONNECTED) {
-    console.log('HTTP头部发送成功');
-    sessionStatus.set(session.uuid, HTTP_STATUS_WAITRESPONSE);
-    // 读取到完整响应头
-    $tunnel.readTo(session, '\r\n\r\n');
-    return false; // 中断当前回调
+function tunnelDidWrite() {
+  if (httpStatus == HTTP_STATUS_CONNECTED) {
+    console.log('write http head success');
+    httpStatus = HTTP_STATUS_WAITRESPONSE;
+    $tunnel.readTo($session, '\x0D\x0A\x0D\x0A'); //读取远端数据直到出现\r\n\r\n
+    return false; //中断wirte callback
   }
   return true;
 }
 
-// 隧道关闭回调
-function tunnelDidClose(session) {
-  sessionStatus.delete(session.uuid);
+function tunnelDidClose() {
   return true;
 }
 
-// 内部方法：构建HTTP头部
-function _writeHttpHeader(session) {
-  const host = session.conHost;
-  const port = session.conPort;
-  const authIndex = createVerify(host);
-  
-  const userAgent = 'Mozilla/5.0 (Linux; Android 12; RMX3300 Build/SKQ1.211019.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36 T7/13.32 SP-engine/2.70.0 baiduboxapp/13.32.0.10 (Baidu; P1 12) NABar/1.0';
-  const xAuth = authIndex.toString();
-  
-  const headers = [
-    `CONNECT ${host}:${port} HTTP/1.1\r\n`,
-    `Host: 153.3.236.22:443\r\n`,
-    `User-Agent: ${userAgent}\r\n`,
-    `Proxy-Connection: Keep-Alive\r\n`,
-    `X-T5-Auth: ${xAuth}\r\n\r\n`
-  ].join('');
-  
-  $tunnel.write(session, headers);
-}
+//Tools
+function _writeHttpHeader() {
+  let conHost = $session.conHost;
+  let conPort = $session.conPort;
 
-// 导出方法供框架调用
-module.exports = {
-  tunnelDidConnected,
-  tunnelTLSFinished,
-  tunnelDidRead,
-  tunnelDidWrite,
-  tunnelDidClose
-};
+  var header = `CONNECT ${conHost}:${conPort}HTTP/1.1\r\nHost: 153.3.236.22:443\r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1 baiduboxapp\r\nX-T5-Auth: 683556433\r\nProxy-Connection: keep-alive\r\n\r\n`;
+  $tunnel.write($session, header);
+}
