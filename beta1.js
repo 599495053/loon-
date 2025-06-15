6347ceeabaa29b66d84208f79aaf454b3a63dad0
599495053 <52025644+599495053@users.noreply.github.com>
2025-06-15 22:13:31 +0800

    Delete beta1.js

--- beta1.js
+++
@@ -1,6 +0,0 @@
-$done({
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1 baiduboxapp',
-    'X-T5-Auth': '683556433'
-  }
-});
